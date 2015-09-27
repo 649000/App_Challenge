2f5e0ff571f5bae007681bae9876da3139f07236
Nazri Yahya <nazri610@gmail.com>
2015-09-27 12:19:04 +0800

    Email Component. Done.

--- Maps/urls.py
+++ Maps/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
-import views
+from . import views
 
 urlpatterns = [
     # /maps/
     url(r'^$', views.index, name='index'),
-   # url(r'^$', views.locationToCoordinates, name='geocode'),
-    url(r'^(?P<location>.*)/$', views.locationToCoordinates, name='geocode')
-
-
+    # url(r'^$', views.locationToCoordinates, name='geocode'),
+    #   url(r'^(?P<location>.*)/$', views.locationToCoordinates, name='geocode'),
+     url(r'^emails/$', views.testMails, name='ea'),
 ]
